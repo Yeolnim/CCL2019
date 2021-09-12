a9be046452b021e780a68dc57e9030e073db9c04
Yeolnim <734975095@qq.com>
2021-09-12 13:49:18 +0800

    Update

--- task1/train.py
+++ task1/train.py
@@ -1,20 +1,22 @@
+import copy
+import jieba
+import gensim
+import numpy as np
 import pandas as pd
+
+from keras.preprocessing.text import Tokenizer
+from keras_preprocessing.sequence import pad_sequences
+from keras.models import Model
+from keras.layers import Input, Dense, LSTM, Dropout, Embedding
 
 train = pd.read_csv('task1_train.csv')
 test = pd.read_csv('task1_development.csv')
-
-import jieba
-import numpy as np
 
 train['sen_cut'] = train['joke'].apply(jieba.lcut)
 
 X_train = train['sen_cut'].apply(lambda x: ' '.join(x)).tolist()
 y_train = pd.get_dummies((np.asarray(train["label"])))
 text = np.array(X_train)
-
-from keras.preprocessing.text import Tokenizer
-from keras_preprocessing.sequence import pad_sequences
-import time
 
 vocab_size = 30000
 maxlen = 25
@@ -31,14 +33,12 @@ print("开始对齐句子序列...")
 X_train = pad_sequences(X_train, maxlen=maxlen, padding='post')
 print("完成！")
 
-import copy
-
-small_word_index = copy.deepcopy(word_index) # 防止原来的字典被改变
+small_word_index = copy.deepcopy(word_index)  # 防止原来的字典被改变
 x = list(t.word_counts.items())
-s = sorted(x, key=lambda p:p[1], reverse=True)
+s = sorted(x, key=lambda p: p[1], reverse=True)
 print("移除word_index字典中的低频词...")
 for item in s[20000:]:
-    small_word_index.pop(item[0]) # 对字典pop
+    small_word_index.pop(item[0])  # 对字典pop
 print("完成！")
 print(len(small_word_index))
 print(len(word_index))
@@ -49,36 +49,27 @@ print(type(y_train))
 print(X_train.shape)
 print(y_train.shape)
 
-import gensim
-
-model_file = 'Tencent_AILab_ChineseEmbedding.vec' # input your file path
+model_file = 'Tencent_AILab_ChineseEmbedding.vec'  # input your file path
 print("加载Word2Vec模型...")
 wv_model = gensim.models.KeyedVectors.load_word2vec_format(model_file, binary=True)
 
-embedding_matrix = np.random.uniform(size=(vocab_size+1,200))
+embedding_matrix = np.random.uniform(size=(vocab_size + 1, 200))
 print("构建embedding_matrix...")
 for word, index in small_word_index.items():
     try:
         word_vector = wv_model[word]
         embedding_matrix[index] = word_vector
     except:
-        print("Word: [",word,"] not in wvmodel! Use random embedding instead.")
+        print("Word: [", word, "] not in wvmodel! Use random embedding instead.")
 print("完成！")
-print("Embedding matrix shape:\n",embedding_matrix.shape)
-
-import keras
-from keras.models import Sequential, Model
-from keras.layers import Input, Dense, GRU, LSTM, Activation, Dropout, Embedding
-from keras.layers import Multiply, Concatenate, Dot
-
-from sklearn.metrics import f1_score
+print("Embedding matrix shape:\n", embedding_matrix.shape)
 
 # lstm
 wv_dim = 200
 n_timesteps = maxlen
 inputs = Input(shape=(maxlen,))
-embedding_sequences = Embedding(vocab_size+1, wv_dim, input_length=maxlen, weights=[embedding_matrix])(inputs)
-lstm = LSTM(128, return_sequences= False)(embedding_sequences)
+embedding_sequences = Embedding(vocab_size + 1, wv_dim, input_length=maxlen, weights=[embedding_matrix])(inputs)
+lstm = LSTM(128, return_sequences=False)(embedding_sequences)
 l = Dense(128, activation="tanh")(lstm)
 l = Dropout(0.5)(l)
 l = Dense(2, activation="softmax")(l)
